10a291b357cbad563ad326548d9cc615b9f84b58
unxok <anderluke707@gmail.com>
2025-09-26 01:51:22 -0700

    setup shadcn

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,22 @@
+import { Button } from "~/components/ui/button";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+	return [
+		{ title: "New React Router App" },
+		{ name: "description", content: "Welcome to React Router!" },
+	];
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
+	return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+	return (
+		<div>
+			hi
+			<Button>hi</Button>
+		</div>
+	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,20 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import mdx from "@mdx-js/rollup";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
-    tailwindcss(),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+	plugins: [
+		mdx(),
+		cloudflare({ viteEnvironment: { name: "ssr" } }),
+		tailwindcss(),
+		reactRouter(),
+		tsconfigPaths(),
+	],
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, "./app"),
+		},
+	},
 });
